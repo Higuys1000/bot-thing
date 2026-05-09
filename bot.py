1f4b252e2b0926bc0cd3589ab68ec20be32e04d5
Higuys1000 <166072269+Higuys1000@users.noreply.github.com>
2026-05-09 15:35:07 -0400

    Testing if update saves cooldown

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.reactions = True
-
+ 
 bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 TARGET_GIFS = [

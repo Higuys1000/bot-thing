055a7a2ebbaf299138c7ff518d01c1fbb06cb1eb
NicheSnoody <taviusxavier@gmail.com>
2026-05-21 17:39:47 -0700

    Update bot.py
    added mill guh

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 TARGET_GIFS = [
     "https://tenor.com/view/jujutsu-kaisen-inumaki-toge-toge-inumaki-inumaki-toge-gif-2839387565091272519",
+    "https://tenor.com/view/a-train-the-boys-gif-27650060",
     "https://klipy.com/gifs/drmanhattan-watchman",
     "https://tenor.com/view/not-very-woke-of-you-nuh-uh-homelander-gen-v-the-boys-gif-5949428463441363002",
     "https://tenor.com/view/vb-dragon-ball-super-finalkamehameha-power-anime-gif-17694754",
